a64b53b136afbc3f0eb0e54cb66889a0dd8bf80b
baumanisjanis4 <baumanis.janis4@gmail.com>
2020-09-08 14:51:34 +0000

    13-truncate-string

--- exercises/00-warm-up/src/13-truncate-string.ts
+++ exercises/00-warm-up/src/13-truncate-string.ts
@@ -1,3 +1,5 @@
 export {};
-
-console.log(truncateString("CODELEX", 4)); // Expected output: CODE
+    let lenght = 4;
+let myString = 'CODELEX';
+let truncateString = myString.substring(0, lenght)
+console.log(truncateString); // Expected output: CODE
