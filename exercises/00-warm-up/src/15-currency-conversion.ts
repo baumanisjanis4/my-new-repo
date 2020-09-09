89ef98f324d2c45a05c649290a12a62ef310cdd9
baumanisjanis4 <baumanis.janis4@gmail.com>
2020-09-09 11:07:32 +0000

    15-currency-convercion

--- exercises/00-warm-up/src/15-currency-conversion.ts
+++ exercises/00-warm-up/src/15-currency-conversion.ts
@@ -8,7 +8,10 @@ export {};
  */
 
 // You are allowed to change only this function
-function convertToUSD() {}
+    
+function convertToUSD(money: number): string {
+return (money *1.4).toFixed(2);
+}
 
 const product = "You don't know JS";
 const price = 19.99;
