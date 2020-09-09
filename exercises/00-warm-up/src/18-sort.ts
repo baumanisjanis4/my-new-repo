45b334635d5aa410a9f320f3998f3544978353e1
baumanisjanis4 <baumanis.janis4@gmail.com>
2020-09-09 13:05:06 +0000

    18-sort

--- exercises/00-warm-up/src/18-sort.ts
+++ exercises/00-warm-up/src/18-sort.ts
@@ -1,6 +1,8 @@
 export {};
 
 const letters = ["a", "n", "c", "e", "z", "f"];
-let sortedLetters; // You are allowed to change only this line
+let sortedLetters:string[] = letters.sort(function(a, b) {
+    return a === b ? 0 : a < b ? -1 : 1;
+  });// You are allowed to change only this line
 
 console.log(sortedLetters); // Expected output: [ 'a', 'c', 'e', 'f', 'n', 'z' ]
